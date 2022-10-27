5a3400fa00342b70cae5bf18bc5d27f9f1aaa545
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-10-26 20:37:29 -0500

    Update

--- PythonBasics/working_with_files.py
+++ PythonBasics/working_with_files.py
@@ -1,23 +1,14 @@
-"""
-try:
-    countlines = countchars = 0
-    file = open('newfile.txt','r')
-    lines = file.readlines()
-    for line in lines:
-        countlines += 1
-        for char in line:
-            countchars += 1
-    file.close()
-    print("Characters in file: ", countchars)
-    print("Lines in file: ", countlines)
-except IOError as error:
-    print("I/O error ocurred:", str(error))
-"""
-try:
-    myfile = open("newfile2.txt", 'wt','r')
-    for i in range(10):
-        myfile.write("line #1" + str(i+1) + "\n")
-    myfile.readlines()
-    myfile.close()
-except IOError as error:
-    print("I/O error ocurred:", str(error))
+from typing import IO
+
+
+def main():
+    try:
+        with open('newfile3.txt','w') as file:
+            file.write("another test line :)")
+    except IOError as e:
+        print("Exception caught: Unable to write to file", e)
+    except Exception as e:
+        print("Another error ocurred", e)
+
+if __name__ == '__main__':
+    main()
