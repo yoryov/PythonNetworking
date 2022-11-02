df24744a97cb5fd68c5503bbf38fc0d5a992cb7a
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-02 00:44:55 -0500

    Update zipfile.py

---
+++ zipfiles.py
@@ -0,0 +1,9 @@
+import zipfile
+
+def list_files_in_zip(filename):
+    with zipfile.ZipFile(filename) as myzip:
+        for zipinfo in myzip.infolist():
+            yield zipinfo.filename
+
+for filename in list_files_in_zip("files.zip"):
+    print(filename)
