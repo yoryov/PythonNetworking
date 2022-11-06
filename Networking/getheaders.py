06c42fe4d88800684396d89d1c0c05293103275f
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-05 21:30:32 -0500

    update...

---
+++ Networking/getheaders.py
@@ -0,0 +1,6 @@
+import urllib.request
+from urllib.request import Request
+
+target="https://linux.org"
+
+USER_AGENT = "Mozilla/4.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36"
