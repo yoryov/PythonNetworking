e8027363b2438bf8a944019b664e8cc71a859ab9
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-07 23:53:57 -0500

    Update getheaders

--- Networking/getheaders.py
+++ Networking/getheaders.py
@@ -1,12 +1,27 @@
 import urllib.request
 from urllib.request import Request
 
-target="https://linux.org"
+target="https://python.org"
 
-USER_AGENT = "Mozilla/4.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 
 def firefox_user_agent():
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', USER_AGENT)]
     urllib.request.install_opener(opener)
     response = urllib.request.urlopen(target)
+    print("Server Information: ")
+    print("----------------------")
+    for header, value in response.getheaders():
+        #if header == ("Server" or "server"): # With this If statement we can get only the web server
+            print(header + ":" + value)
+    
+    request = Request(target)
+    request.add_header('User-agent', USER_AGENT)
+    print("\nRequest headers")
+    print("----------------------")
+    for header, value in request.header_items():
+        print(header + ":" + value)
+
+if __name__ == '__main__':
+    firefox_user_agent()
