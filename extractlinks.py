e2a39703ca7beebc2ae1a90c4f3eee96730d34b6
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-09 21:02:57 -0500

    some tests...

--- extractlinks.py
+++ extractlinks.py
@@ -3,13 +3,16 @@ import re
 
 url = input("URL: ")
 var = requests.get(url).text
-print("Links: ")
-print("###############")
+print(var)
 for link in re.findall("<a (.*)></a>",var):
+    print(link)
+"""
+
     for a in link.split():
         if re.findall("href=(.*)",a):
-            url_link = a[0:-1].replace("href=","")
-            if(url_link.startswith("http")):
-                print(url_link)
+            url_link = a[0:-1].replace("href=\"","")
+            if(a.startswith("http")):
+                print(a)
             else:
                 print(url+url_link)
+"""
