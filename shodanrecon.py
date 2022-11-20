bd4826ef0c35dcbfbbe76b345084d0ef26252237
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-20 17:44:16 -0500

    Update with argparse for "target" or "search"

--- shodanrecon.py
+++ shodanrecon.py
@@ -1,11 +1,40 @@
 import shodan
 import os
+import sys
+import argparse
+import socket
 
 SHODAN_KEY = os.environ['shodanapikey']
 recon = shodan.Shodan(SHODAN_KEY)
 
-try:
-    result = recon.search('Apache')
-    print('results:',result.items())
-except Exception as e:
-    print(str(e))
+parser = argparse.ArgumentParser(description='Shodan API Search!')
+
+# adding 2 arguments
+
+parser.add_argument("--target", "-t", dest="target", help="Target IP or Domain",required=None)
+parser.add_argument("--search", "-s", dest="search", help="Normal Shodan Search", required=None)
+parsed_args = parser.parse_args() # Parsing the arguments
+
+
+if sys.argv[1] == '--search' or '-s':
+    try:
+        results = recon.search(parsed_args.search)
+        print('Results: %s' %results['total'])
+        for result in results['matches']:
+            print('IP: %s' %result['ip_str'])
+            print(result['data'])
+    except shodan.APIError as e:
+        print(str(e))
+
+if sys.argv[1] == '--target' or '-t':
+    try:
+        hostname = socket.gethostbyname(parsed_args.target)
+        results = recon.host(hostname)
+        print("""IP: %s \nOrganization: %s \nOS: %s """ 
+        % (results['ip_str'], results.get('org','n/a'), results.get('os', 'n/a')))
+        for item in results['data']:
+            print("""Port: %s \nBanner: %s """ 
+            % (item['port'], item['data']))
+    except shodan.APIError as e:
+        print(str(e))
+
